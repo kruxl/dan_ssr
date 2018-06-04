0b9353f46dafdbd89a7e8dc045b2ff57a71df980
kruxl <andrei.soimareanu@gmail.com>
2018-06-04 23:11:00 +0300

    changed config and some readme

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'page-transitions-travelapp',
+    title: 'Dan SSR Temp Title',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'Native-like Page Transitions with Vue and Nuxt, A Travel App'
+        content: 'Dan SSR Temp Description'
       }
     ],
     link: [
